269ec23418189124458ee441b151980e00eab8e5
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2024-12-10 15:07:13 +0530

    Q5

---
+++ patterns.js
@@ -0,0 +1,16 @@
+/*Write a JavaScript program to construct the following pattern, using a nested for loop.
+* 
+* * 
+* * * 
+* * * * 
+* * * * **/
+
+
+for (let i = 1; i <= 5; i++) {
+  
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '* '; 
+    }
+    console.log(row.trim()); 
+}
